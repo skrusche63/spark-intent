62de5b681152d67c25c4a955f818e255bf2130ad
skrusche63 <skrusche63@googlemail.com>
2014-11-25 16:57:13 +0100

    RedisSink added.

---
+++ src/main/scala/de/kp/spark/intent/sink/RedisSink.scala
@@ -0,0 +1,71 @@
+package de.kp.spark.intent.sink
+/* Copyright (c) 2014 Dr. Krusche & Partner PartG
+ * 
+ * This file is part of the Spark-Intent project
+ * (https://github.com/skrusche63/spark-intent).
+ * 
+ * Spark-Intent is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Spark-Intent is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with
+ * Spark-Intent. 
+ * 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.util.Date
+
+import de.kp.spark.core.model._
+import de.kp.spark.core.redis.RedisClient
+
+import de.kp.spark.intent.model._
+
+import scala.collection.JavaConversions._
+
+class RedisSink {
+
+  val client  = RedisClient()
+  val service = "intent"
+  
+  def addModel(req:ServiceRequest,model:String) {
+   
+    val now = new Date()
+    val timestamp = now.getTime()
+    
+    val k = "model:" + service + ":" + req.data("uid")
+    val v = "" + timestamp + ":" + model
+    
+    client.zadd(k,timestamp,v)
+    
+  }
+  
+  def modelExists(uid:String):Boolean = {
+
+    val k = "model:" + service + ":" + uid
+    client.exists(k)
+    
+  }
+  
+  def model(uid:String):String = {
+
+    val k = "model:" + service + ":" + uid
+    val models = client.zrange(k, 0, -1)
+
+    if (models.size() == 0) {
+      null
+    
+    } else {
+      
+      val last = models.toList.last
+      last.split(":")(1)
+      
+    }
+  
+  }
+
+}
